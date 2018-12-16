667a0575e8cc5a8ea6f7de5e8ee78cbfdf8d1c8f
KostekF <filip.kostecki@student.put.poznan.pl>
2018-12-16 17:56:00 +0100

    Changed: formula of creating depth image,focals,center points

--- src/PointCloud2ToDepth.cpp
+++ src/PointCloud2ToDepth.cpp
@@ -7,10 +7,62 @@
             depthImage_.release();
         
         //nadanie pustemu obrazowi glebi wymiarow
-        int depthImageHeight=ptr_cloud->height /4;// dzięki funkcji Multi-Echo otrzymujemy 4 echa, z czego korzystam z jednego, głównego
+        int depthImageHeight=ptr_cloud->height /4;// dzięki funkcji Multi-Echo otrzymujemy 4 echa, z czego korzystam z jednego, głównego  //SICK
+        //int depthImageHeight=ptr_cloud->height; //KINECT
         int depthImageWidth=ptr_cloud->width;//924
         depthImage_.create(depthImageHeight,depthImageWidth,CV_32F);  
+
+      
         
+        const int m_to_mm=1000;
+               
+                
+        for(int v=0;v<depthImage_.rows;++v)
+        
+        {
+            
+            for(int u=0;u<depthImage_.cols;++u)
+        
+            {
+               // ROS_INFO("P.z= %f\n",ptr_cloud->points.at(count).z*m_to_mm);
+               
+                //ROS_INFO("height of pcl=%d",ptr_cloud->height);
+               // float x=(ptr_cloud->points.at(v*924 +u).x-cx);
+               // float y=ptr_cloud->points.at(v*924 +u).y-cy;
+              //  float x=ptr_cloud->points.at(v*924 +u).x;
+              //  float y=ptr_cloud->points.at(v*924 +u).y;
+                
+               // float image_x=(x-cx_mm);
+               // float image_y=(y-cy_mm);
+                //ROS_INFO("image_x=%f",image_x);
+              //  ROS_INFO("image_y=%f",image_y);
+                
+               // ROS_INFO("x=%f",x);
+              // ROS_INFO("y=%f",ptr_cloud->points.at(v*924 +u).z*m_to_mm);
+                float valOfDepthImagePixel=ptr_cloud->points.at(v*924 +u).x*m_to_mm;
+           
+                 if(valOfDepthImagePixel>0)
+                {
+                depthImage_.at<float>(v,u)=valOfDepthImagePixel; //przypisanie odleglosci do danego piksela obrazu glebi 
+                }
+                else
+                    depthImage_.at<float>(v,u)=0;
+            
+             // ROS_INFO("P.z=%f",ptr_cloud->points.at(v*924 +10).z);
+                  // ROS_INFO("P.x=%f",ptr_cloud->points.at(v*924 +u).x);
+                    //ROS_INFO("P.x=%f",ptr_cloud->points.at(v*924 +u).x);
+              // ROS_INFO("depth=%f", depthImage_.at<float>(v,u));
+               
+
+             
+            } 
+                
+        }
+                
+            
+        
+        
+/*        
         int count =0;
     //   #pragma omp parallel for //multithreading
 
@@ -30,11 +82,10 @@
                 
                 ++count;
             }
-    //  std::cout<<depthImage_.rows<<std::endl;
+  */
+  //  std::cout<<depthImage_.rows<<std::endl;
         depthImage_.convertTo(depthImage_,CV_16U);
-            
-    }
-     
+     }
   void PointCloud2ToDepth::cloud_cb(const sensor_msgs::PointCloud2ConstPtr& input)
   {
    
@@ -88,7 +139,9 @@
   }
 
  
- PointCloud2ToDepth::PointCloud2ToDepth() : cloud_topic_("sick_mrs6xxx/cloud"),depthImage_topic_("depthFromPcl/image_raw"),camInfo_topic_("depthFromPcl/camera_info")
+
+    //PointCloud2ToDepth::PointCloud2ToDepth() : cloud_topic_("camera/depth/points"),depthImage_topic_("camera/depth/newImage"),camInfo_topic_("camera/depth/camera_info") //KINECT
+    PointCloud2ToDepth::PointCloud2ToDepth() : cloud_topic_("sick_mrs6xxx/cloud"),depthImage_topic_("depthFromPcl/image_raw"),camInfo_topic_("depthFromPcl/camera_info") //SICK
   {
       ///DO i need two node handlers for two topics? or another node handle for camera info?
       
